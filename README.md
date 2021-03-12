a33ac8487626c4ee17d78a8da711b76e32ab3a26
Etienne Dijon <edijon@rpb-tech.com>
2021-03-12 13:46:18 +0100

    chore(install.sh): clean code

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
-# kitty-custom
+# kitty-set-my-conf
+
+Set my configuration for kitty.
+> open-source GPU based terminal
+
+- `kitty` repository : https://github.com/kovidgoyal/kitty
+- `kitty-themes` repository : https://github.com/dexpota/kitty-themes
+- You are free to use it on your own risk.
+
 
 ## Ubuntu
 
-`sudo apt update && sudo apt install kitty`
+```bash
+sudo apt update && sudo apt install kitty
+```
 
 ## Centos 8
 
-`sudo dnf install --refresh kitty`
+```bash
+sudo dnf install --refresh kitty
+```
--- install.sh
+++ install.sh
@@ -1,9 +1,30 @@
 #! /bin/bash
 
-#: Install configuration file
-mkdir -p ~/.config/kitty
-cp ./kitty.conf ~/.config/kitty/
+SCRIPT_DIRECTORY=$(readlink -f $0)
+SCRIPT_DIRECTORY=$(cd $(dirname $SCRIPT_DIRECTORY); pwd)
 
-#: Install kitty themes
-git clone --depth 1 https://github.com/dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
-ln -sf ~/.config/kitty/kitty-themes/themes/ayu_mirage.conf ~/.config/kitty/theme.conf
+main() {
+    install_configuration_file
+    get_kitty_themes_repository
+    set_kitty_theme
+}
+
+install_configuration_file() {
+    configuration_file="$SCRIPT_DIRECTORY/kitty.conf"
+    configuration_directory="$HOME/.config/kitty"
+
+    mkdir -p $configuration_directory && \
+    cp $configuration_file $configuration_directory/
+}
+
+get_kitty_themes_repository() {
+    git clone --depth 1 \
+        https://github.com/dexpota/kitty-themes.git \
+        $HOME/.config/kitty/kitty-themes
+}
+
+set_kitty_theme() {
+    ln -sf $HOME/.config/kitty/kitty-themes/themes/ayu_mirage.conf $HOME/.config/kitty/theme.conf
+}
+
+main $@
