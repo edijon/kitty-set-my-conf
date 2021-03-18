cb986516a1f89ed6020916bec48c2d2a623582bd
edijon <36565506+edijon@users.noreply.github.com>
2021-03-18 12:51:52 +0100

    style(install): add blank lines

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
+
 SCRIPT_DIRECTORY=$(readlink -f $0)
 SCRIPT_DIRECTORY=$(cd $(dirname $SCRIPT_DIRECTORY); pwd)
+
 
 main() {
     install_configuration_file
     get_kitty_themes_repository
     set_kitty_theme
 }
+
 
 install_configuration_file() {
     configuration_file="$SCRIPT_DIRECTORY/kitty.conf"
@@ -17,14 +20,17 @@ install_configuration_file() {
     cp $configuration_file $configuration_directory/
 }
 
+
 get_kitty_themes_repository() {
     git clone --depth 1 \
         https://github.com/dexpota/kitty-themes.git \
         $HOME/.config/kitty/kitty-themes
 }
 
+
 set_kitty_theme() {
     ln -sf $HOME/.config/kitty/kitty-themes/themes/ayu_mirage.conf $HOME/.config/kitty/theme.conf
 }
+
 
 main $@
